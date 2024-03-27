b80644aebfa2fdf930286df6ecee7e7237026832
João Cardoso <brendakailanidasilva@gmail.com>
2024-03-27 20:49:56 -0300

    Alterado texto por classe

---
+++ semana 8/ex-02/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    const elementos = document.getElementsByClassName('alterarClasse');
+
+    for (let i = 0; i < elementos.length; i++) {
+        elementos[i].textContent ='Here a oink, there a oink, everywhere a oink-oink';
+    }
+};
